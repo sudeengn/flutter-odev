e2ef3c54233bc1350cf4d254f7a6793418ec07a4
sudeengn <sudenur.engin@icloud.com>
2024-06-10 14:39:39 +0300

    !

--- lib/Presentation/resources/language_manager.dart
+++ lib/Presentation/resources/language_manager.dart
@@ -5,10 +5,11 @@ enum LanguageType { ENGLISH, ARABIC }
 const String ARABIC = "ar";
 const String ENGLISH = "en";
 
-const Locale ARABIC_LOCAL = Locale("tr", "TR");
-const Locale ENGLISH_LOCAL = Locale("en", "US");
+const Locale ARABIC_LOCAL = Locale("ar","EG");
+const Locale ENGLISH_LOCAL = Locale("en","US");
 
 const String LOCALISATIONS_PATH = "assets/translations";
+
 
 extension LanguageTypeExtension on LanguageType {
   String getValue() {
